e6bb29c4c6228748135fd7bc28f5ee27326ad32e
senemozcan <elifsenemozcan@gmail.com>
2022-08-28 20:00:40 +0300

    Initial commit

---
+++ src/BasitHesapMakinesi.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class BasitHesapMakinesi {
+    public static void main(String[] args) {
+        int n1, n2, select;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Ilk Sayiyi Giriniz: ");
+        n1 = input.nextInt();
+        System.out.print("Ikinci Sayiyi Giriniz: ");
+        n2 = input.nextInt();
+
+        System.out.println("1-Toplama\n2-Cikarma\n3-Carpma\n4-Bolme");
+        System.out.print("Seciminiz : ");
+        select = input.nextInt();
+
+        switch (select) {
+            case 1:
+                System.out.println("Toplam = " + (n1 + n2));
+                break;
+            case 2:
+                System.out.println("Cikarma = " + (n1 - n2));
+                break;
+            case 3:
+                System.out.println("Carpma = " + (n1 * n2));
+                break;
+            case 4:
+                switch (n2) {
+                    case 0:
+                        System.out.println("Bir sayı sıfıra bolunemez !!");
+                        break;
+                    default:
+                        System.out.println("Bolme = " + (n1 / n2));
+                }
+                break;
+            default:
+                System.out.println("Yanlıs sayı girdiniz. Tekrar deneyiniz.");
+        }
+        input.close();
+    }
+}
